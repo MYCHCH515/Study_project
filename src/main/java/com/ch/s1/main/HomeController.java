86208810b6120fb01ff00395ac9df1b8c0976bcd
MYCHCH515 <nam1738@naver.com>
2020-12-16 00:30:59 +0900

    initial commit
    pom.xml app.properties 파일등 초기설정

---
+++ src/main/java/com/ch/s1/main/HomeController.java
@@ -0,0 +1,13 @@
+package com.ch.s1.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+}
